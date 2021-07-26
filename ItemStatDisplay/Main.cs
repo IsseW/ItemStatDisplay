e3f31bdfa28ea82ede59d7fc343e0b0a383a3a4b
IsseW <isidornie@gmail.com>
2021-07-27 00:26:44 +0200

    Made it easier to integrate with other mods

--- ItemStatDisplay/Main.cs
+++ ItemStatDisplay/Main.cs
@@ -11,7 +11,6 @@ using TMPro;
 namespace ItemStatDisplay
 {
 
-
     [BepInPlugin(Guid, Name, Version)]
     public class MainClass : BaseUnityPlugin
     {
@@ -39,7 +38,13 @@ namespace ItemStatDisplay
              
             harmony.PatchAll(typeof(PowerUpUIPatch));
             harmony.PatchAll(typeof(ExtraItemInfo)); 
-            harmony.PatchAll(typeof(ExtraPowerupInfo));
+            harmony.PatchAll(typeof(ExtraPowerupInfo)); 
+        }
+
+        void RegisterPowerUp(KeyValuePair<string, Func<int, string>> def)
+        {
+            log.LogMessage("Added powerup: " + def.Key);
+            ExtraPowerupInfo.Add(def.Key, def.Value);
         }
     }
 
@@ -71,10 +76,10 @@ namespace ItemStatDisplay
 
     public static class Helper
     {
-        public static float CD(int amount, float scaleSpeed, float maxValue)
+        public static float CD(int count, float scaleSpeed, float maxValue)
         {
             float f = 2.71828f;
-            return (1f - Mathf.Pow(f, (float)(-(float)amount) * scaleSpeed)) * maxValue;
+            return (1f - Mathf.Pow(f, (float)(-(float)count) * scaleSpeed)) * maxValue;
         }
 
         public static string Percent(float f)
@@ -163,31 +168,31 @@ namespace ItemStatDisplay
         }
         static void AddDefault()
         {
-            Add("Dumbbell", (amount) => "+" + (amount / 10f).ToString("N1") + " strength");
-            Add("Berserk", (amount) => "+" + (amount / 100f).ToString("N2") + " strength per % of missing hp");
-            Add("Peanut Butter", (amount) => "+" + (amount * 0.15f).ToString("N2") + " stamina");
-            Add("Adrenaline", (amount) => "While active gain " + Helper.Percent(Helper.CD(amount, 1f, 2f)) + " stamina, movementspeed and attackspeed");
-            Add("Sneaker", (amount) => Helper.Percent(1f + Helper.CD(amount, 0.08f, 1.75f)) + " movement speed");
-            Add("Sniper Scope", (amount) => Helper.Percent(Helper.CD(amount, 0.14f, 0.15f)) + " chance to deal " + Helper.Percent(Helper.CD(amount, 0.25f, 50)) + " damage");
-            Add("Blue Pill", (amount) => "+" + (amount * 10) + " shield");
-            Add("Robin Hood Hat", (amount) => Helper.Percent(1f + Helper.CD(amount, 0.06f, 2f)) + " faster bow draw speed, faster arrow speed and arrow damage");
-            Add("Checkered Shirt", (amount) => Helper.Percent(1 + Helper.CD(amount, 0.3f, 4f)) + " resource damage");
-            Add("Dracula", (amount) => "Gain " + amount + " max hp on kill, stacks up to " + (amount * 40) + " bonus max hp");
-            Add("Piggybank", (amount) => Helper.Percent((1f + Helper.CD(amount, 0.15f, 1.25f))) + " loot dropped from mobs and resources");
-            Add("Knuts Hammer", (amount) => Helper.Percent(Helper.CD(amount, 0.12f, 0.4f)) + " chance to summon lightning that deals +" + Helper.Percent(2f + Helper.CD(amount, 0.12f, 1f)) + " of the damage dealt");
-            Add("Crimson Dagger", (amount) => "Heal for " + Helper.Percent(Helper.CD(amount, 0.1f, 0.5f)) + " of damage dealt");
-            Add("Bulldozer", (amount) => Helper.Percent(Helper.CD(amount, 0.15f, 1f)) + " chance to knock back target");
-            Add("Jetpack", (amount) => Helper.Percent(1f + Helper.CD(amount, 0.075f, 2.5f)) + " jump height");
-            Add("Juice", (amount) => Helper.Percent(1f + Helper.CD(amount, 0.3f, 1f)) + " attack speed for 2 seconds after a crit");
-            Add("Spooo Bean", (amount) => Helper.Percent((1f - Helper.CD(amount, 0.2f, 0.5f))) + " hunger drain");
-            Add("Red Pill", (amount) => "+" + (amount * 10) + " max hp");
-            Add("Broccoli", (amount) => (100 + amount * 5) + "% healing");
-            Add("Wings of Glory", (amount) => Helper.Percent((1f + Helper.CD(amount, 0.45f, 2.5f))) + " damage while falling");
-            Add("Janniks Frog", (amount) => amount + " extra jumps");
-            Add("Enforcer", (amount) => "+" + (5f * Helper.CD(amount, 0.4f, 2f)).ToString("N1") + "% damage for each unit moved per second");
-            Add("Danis Milk", (amount) => "+" + (Helper.CD(amount, 0.1f, 40f)).ToString("N1") + " bonus armor");
-            Add("Horseshoe", (amount) => "+" + Helper.Percent(Helper.CD(amount, 0.08f, 0.9f)) + " crit chance");
-            Add("Orange Juice", (amount) => Helper.Percent((1f + Helper.CD(amount, 0.12f, 1f))) + " attack speed");
+            Add("Dumbbell", (count) => "+" + (count / 10f).ToString("N1") + " strength");
+            Add("Berserk", (count) => "+" + (count / 100f).ToString("N2") + " strength per % of missing hp");
+            Add("Peanut Butter", (count) => "+" + (count * 0.15f).ToString("N2") + " stamina");
+            Add("Adrenaline", (count) => "While active gain " + Helper.Percent(Helper.CD(count, 1f, 2f)) + " stamina, movementspeed and attackspeed");
+            Add("Sneaker", (count) => Helper.Percent(1f + Helper.CD(count, 0.08f, 1.75f)) + " movement speed");
+            Add("Sniper Scope", (count) => Helper.Percent(Helper.CD(count, 0.14f, 0.15f)) + " chance to deal " + Helper.Percent(Helper.CD(count, 0.25f, 50)) + " damage");
+            Add("Blue Pill", (count) => "+" + (count * 10) + " shield");
+            Add("Robin Hood Hat", (count) => Helper.Percent(1f + Helper.CD(count, 0.06f, 2f)) + " faster bow draw speed, faster arrow speed and arrow damage");
+            Add("Checkered Shirt", (count) => Helper.Percent(1 + Helper.CD(count, 0.3f, 4f)) + " resource damage");
+            Add("Dracula", (count) => "Gain " + count + " max hp on kill, stacks up to " + (count * 40) + " bonus max hp");
+            Add("Piggybank", (count) => Helper.Percent((1f + Helper.CD(count, 0.15f, 1.25f))) + " loot dropped from mobs and resources");
+            Add("Knuts Hammer", (count) => Helper.Percent(Helper.CD(count, 0.12f, 0.4f)) + " chance to summon lightning that deals +" + Helper.Percent(2f + Helper.CD(count, 0.12f, 1f)) + " of the damage dealt");
+            Add("Crimson Dagger", (count) => "Heal for " + Helper.Percent(Helper.CD(count, 0.1f, 0.5f)) + " of damage dealt");
+            Add("Bulldozer", (count) => Helper.Percent(Helper.CD(count, 0.15f, 1f)) + " chance to knock back target");
+            Add("Jetpack", (count) => Helper.Percent(1f + Helper.CD(count, 0.075f, 2.5f)) + " jump height");
+            Add("Juice", (count) => Helper.Percent(1f + Helper.CD(count, 0.3f, 1f)) + " attack speed for 2 seconds after a crit");
+            Add("Spooo Bean", (count) => Helper.Percent((1f - Helper.CD(count, 0.2f, 0.5f))) + " hunger drain");
+            Add("Red Pill", (count) => "+" + (count * 10) + " max hp");
+            Add("Broccoli", (count) => (100 + count * 5) + "% healing");
+            Add("Wings of Glory", (count) => Helper.Percent((1f + Helper.CD(count, 0.45f, 2.5f))) + " damage while falling");
+            Add("Janniks Frog", (count) => count + " extra jumps");
+            Add("Enforcer", (count) => "+" + (5f * Helper.CD(count, 0.4f, 2f)).ToString("N1") + "% damage for each unit moved per second");
+            Add("Danis Milk", (count) => "+" + (Helper.CD(count, 0.1f, 40f)).ToString("N1") + " bonus armor");
+            Add("Horseshoe", (count) => "+" + Helper.Percent(Helper.CD(count, 0.08f, 0.9f)) + " crit chance");
+            Add("Orange Juice", (count) => Helper.Percent((1f + Helper.CD(count, 0.12f, 1f))) + " attack speed");
         }
 
         public static void Add(string name, Func<int, string> function)
